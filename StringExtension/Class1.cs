4d55bd452d2b5436830c1b3a7d7d3bf8cf51dd84
KidoAklilu <Kidoget321@gmail.com>
2025-03-28 19:35:52 -0400

    Initial commit for StringExtensions

---
+++ StringExtension/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace StringExtensions
+{
+    public static class StringHelper
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
